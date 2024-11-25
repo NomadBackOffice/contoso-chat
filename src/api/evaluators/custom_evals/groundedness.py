a91fde4062a3de5591505d7a67afa7ee11d53ee9
HW91 <63923099+HW91@users.noreply.github.com>
2024-11-25 00:49:41 -0500

    Update groundedness.py

--- src/api/evaluators/custom_evals/groundedness.py
+++ src/api/evaluators/custom_evals/groundedness.py
@@ -28,7 +28,7 @@ def groundedness_evaluation(
   # execute the prompty file
   model_config = {
         "azure_endpoint": os.environ["AZURE_OPENAI_ENDPOINT"],
-        "api_version": os.environ["AZURE_OPENAI_API_VERSION"],
+        "api_version": os.environ[2024-07-18],
   }
   result = prompty.execute(
     "groundedness.prompty", 
